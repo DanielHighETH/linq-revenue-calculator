18e6dd664897f7abe217ddb0f7261c5611720932
DanielHighETH <danielhigheth@gmail.com>
2023-09-14 02:08:22 +0800

    changed font in navbar and footer

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,10 +4,9 @@ export default function Navbar() {
 
     return (
         <nav className="flex items-center justify-between flex-wrap">
-            <div className="flex items-center flex-shrink-0 text-white gap-5 font-bold text-lg uppercase">
+            <div className="flex items-center flex-shrink-0 text-white gap-5 font-bold text-lg uppercase font-header">
                 <Link href="/">Home</Link>
                 <Link href="https://dune.com/whale_hunter/linq-protocol" target='blank'>Data</Link>
-                <Link href="/about">About</Link>
                 </div>
         </nav>
     );
